41a679c1150e8708bf0d1f4cb3a5fce836fe10c8
allygetter <134484235+allygetter@users.noreply.github.com>
2026-07-23 18:51:51 +0300

    Update build.gradle.kts

--- mobile/android/build.gradle.kts
+++ mobile/android/build.gradle.kts
@@ -1,49 +1,26 @@
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-android {
-    namespace = "com.example.pesapop_app"
-
-    compileSdk = flutter.compileSdkVersion
-
-    defaultConfig {
-        applicationId = "com.example.pesapop_app"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    // FIX FOR flutter_local_notifications
-    compileOptions {
-        isCoreLibraryDesugaringEnabled = true
-
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
-        }
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
-flutter {
-    source = "../.."
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory =
+        newBuildDir.dir(project.name)
+
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    evaluationDependsOn(":app")
 }
 
-dependencies {
-    // Required by flutter_local_notifications
-    coreLibraryDesugaring(
-        "com.android.tools:desugar_jdk_libs:2.1.4"
-    )
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
